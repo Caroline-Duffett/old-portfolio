1c8150a7c730807726acd4088d43f63c2b498317
CarolineDuffett <ckduffett@gmail.com>
2022-04-27 19:58:35 -0400

    rough modal working

--- project_1/projects/projects.js
+++ project_1/projects/projects.js
@@ -1,1 +1,21 @@
 console.log('pink');
+
+$(() => {
+
+  const $opensModal = $('.open') //what makes modal open
+  const $modal = $('#modal') //grabs the modal div
+  const $closeModal = $('#close')
+
+
+  const open = () => {
+    $modal.css('display', 'block')
+  }
+
+  const close =  () => {
+    $modal.css('display', 'none')
+  }
+
+  $opensModal.on('click', open)
+  $closeModal.on('click', close)
+
+})
