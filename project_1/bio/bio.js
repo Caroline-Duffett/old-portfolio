e869a2363ec5c4ab33267a3d96bbdc6440e89c5e
CarolineDuffett <ckduffett@gmail.com>
2022-04-27 15:05:02 -0400

    added about me

--- project_1/bio/bio.js
+++ project_1/bio/bio.js
@@ -1,1 +1,90 @@
 console.log('Hi');
+
+// /*------------ #flexbox ------------*/
+// #flexbox1 {
+//   display: flex;
+//   background-color: blue;
+//   flex-wrap: wrap;
+//   flex-direction: row;
+//   margin-top: 5em;
+//   height: 43em;
+//   width: 100%;
+//   justify-content: space-evenly;
+// }
+//
+// #flexbox3 {
+//   display: flex;
+//   background-color: white;
+//   flex-wrap: wrap;
+//   flex-direction: row;
+//   width: 100%;
+//   height: 100%;
+// }
+//
+// #flexbox2 {
+//   display: flex;
+//   background-color: white;
+//   flex-wrap: wrap;
+//   flex-direction: row;
+//   width: 100%;
+//   justify-content: space-evenly;
+// }
+//
+// #about {
+//   background-color: lightblue;
+//   margin: 2% 1% 1% 1%;
+//   width: 100%;
+//   padding-left: 2%;
+//   padding-right: 2%;
+// }
+//
+// #pictures {
+//   background-color: lightblue;
+//   margin: 1% 1% 1% 1%;
+//   width: 100%;
+//   padding-left: 2%;
+//   padding-right: 2%;
+// }
+//
+// h3 {
+//   text-align: center;
+// }
+//
+// p {
+//   font-size: 1.2em;
+// }
+//
+// li {
+//   margin-left: 11%;
+//   padding-bottom: 3%;
+//   /*helped me get icons for the lists https://fontawesome.com/docs/web/style/lists. See hmtl */
+//   /*helped me animate the icons https://fontawesome.com/v6/docs/web/style/animate*/
+// }
+//
+// #interests {
+//   background-color: pink;
+//   width: 13em;
+//   height: 12.5em;
+//   margin: 1% 1% 1% 1%;
+//   padding-left: 3%;
+//   padding-right: 3%;
+// }
+//
+// #movies {
+//   background-color: pink;
+//   width: 13em;
+//   height: 12.5em;
+//   margin: 1% 1% 1% 1%;
+//   padding-left: 3%;
+//   padding-right: 3%;
+// }
+//
+// #foods {
+//   background-color: pink;
+//   width: 13em;
+//   height: 12.5em;
+//   margin: 1% 1% 1% 1%;
+//   padding-left: 3%;
+//   padding-right: 3%;
+// }
+// /*----------------------------------*/
