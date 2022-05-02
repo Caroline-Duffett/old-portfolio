aef440d0038d9e6987d01b3ad4e01da6b3e9f4ad
CarolineDuffett <ckduffett@gmail.com>
2022-05-02 10:57:05 -0400

    attempted getting player name inputs

--- project_1/projects/tic_tac_toe/tic_tac_toe.js
+++ project_1/projects/tic_tac_toe/tic_tac_toe.js
@@ -164,6 +164,117 @@
 
 
 //------------------- WAY 4 ---------------------------//
+// //Function to get player 1 name input and display player 1 name
+// const player1Name = () => {
+//   let playerOne = document.getElementById('name1').value;
+//   document.getElementById('p1').innerHTML = `Player One: ${playerOne}`
+// }
+//
+// //Function changes text from play 1 to player 2
+// const removePlayer1 = () => {
+//   const remove1 = document.getElementById('text1').innerHTML = `Player Two: `
+// }
+//
+// //Function changes id from name1 to name 2
+// const idChange = () => {
+//   const change = document.getElementById('name1').id = 'name2';
+// }
+//
+// //Function changes id from p1Btn to p2Btn
+// const btnChange = () => {
+//   const changeBtn = document.getElementById('p1Btn').id = 'p2Btn';
+// }
+//
+// //Function change
+// const functionChange = () => {
+//   document.getElementById('p1Btn').onclick = btn2Functions()
+// }
+//
+// //calls all player one functions on click
+// const btn1Functions = () => {
+//   player1Name();
+//   removePlayer1();
+//   idChange();
+//   btnChange();
+//   functionChange();
+// }
+//
+// //Function to get player 2 name input and display player 2 name
+// const player2Name = () => {
+//   let playerTwo = document.getElementById('name2').value;
+//   document.getElementById('p2').innerHTML = `Player Two: ${playerTwo}`
+// }
+//
+// const btn2Functions = () => {
+//   player2Name();
+// }
+
+
+
+
+//-------------- WAY 5 ----------------//
+// //Function to get player 1 name input and display player 1 name
+// const player1Name = () => {
+//   let playerOne = document.getElementById('name1').value;
+//   document.getElementById('p1').innerHTML = `Player One: ${playerOne}`
+// }
+//
+// //Function changes text from play 1 to player 2
+// const removePlayer1 = () => {
+//   const remove1 = document.getElementById('text1').innerHTML = `Player Two: `
+// }
+//
+// //Function changes id from name1 to name 2
+// const idChange = () => {
+//   const change = document.getElementById('name1').id = 'name2';
+// }
+//
+// //Function to delete p1Btn create p2Btn
+// const removep1Btn = () => {
+//   let removeBtn1 = document.getElementById('p1Btn')
+//   removeBtn1.remove();
+// }
+//
+// //Function to create p2Btn and append it
+// const makep2Btn = () => {
+//   const p2Btn = document.createElement('button');
+//   p2Btn.innerText = 'Enter';
+//   p2Btn.id = 'p2Btn';
+//   p2Btn.onclick = btn2Functions();
+//   const appendDiv = document.getElementById('p1inputDiv');
+//   appendDiv.appendChild(p2Btn);
+// }
+//
+// //Function change
+// const functionChange = () => {
+//   document.getElementById('p2Btn').onclick = btn2Functions()
+// }
+//
+// //calls all player one functions on click
+// const btn1Functions = () => {
+//   player1Name();
+//   removePlayer1();
+//   idChange();
+//   removep1Btn();
+//   makep2Btn();
+//   functionChange();
+// }
+//
+// //Function to get player 2 name input and display player 2 name
+// const player2Name = () => {
+//   let playerTwo = document.getElementById('name2').value;
+//   document.getElementById('p2').innerHTML = `Player Two: ${playerTwo}`
+// }
+//
+// const btn2Functions = () => {
+//   player2Name();
+// }
+
+
+//--------------------- WAY 6 -------------------//
+//Hide player 2 button
+document.getElementById('p2Btn').style.display = 'none';
+
 //Function to get player 1 name input and display player 1 name
 const player1Name = () => {
   let playerOne = document.getElementById('name1').value;
@@ -180,14 +291,25 @@ const idChange = () => {
   const change = document.getElementById('name1').id = 'name2';
 }
 
-//Function changes id from p1Btn to p2Btn
-const btnChange = () => {
-  const changeBtn = document.getElementById('p1Btn').id = 'p2Btn';
+//Function to delete p1Btn create p2Btn
+const removep1Btn = () => {
+  let removeBtn1 = document.getElementById('p1Btn')
+  removeBtn1.remove();
+}
+
+//Function to create p2Btn and append it
+const makep2Btn = () => {
+  const p2Btn = document.createElement('button');
+  p2Btn.innerText = 'Enter';
+  p2Btn.id = 'p2Btn';
+  p2Btn.onclick = btn2Functions();
+  const appendDiv = document.getElementById('p1inputDiv');
+  appendDiv.appendChild(p2Btn);
 }
 
 //Function change
 const functionChange = () => {
-  document.getElementById('p1Btn').onclick = btn2Functions()
+  document.getElementById('p2Btn').onclick = btn2Functions()
 }
 
 //calls all player one functions on click
@@ -195,7 +317,8 @@ const btn1Functions = () => {
   player1Name();
   removePlayer1();
   idChange();
-  btnChange();
+  removep1Btn();
+  makep2Btn();
   functionChange();
 }
 
